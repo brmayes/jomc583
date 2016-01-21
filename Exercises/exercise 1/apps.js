2760b0fb8c51df34a710bbe3274a51e9ec8238a0
brmayes <brmayes@live.unc.edu>
2016-01-21 10:19:04 -0500

    finished pt. 1-4 in exercise

--- Exercises/exercise 1/apps.js
+++ Exercises/exercise 1/apps.js
@@ -16,27 +16,78 @@ function farToCel (farTemp) {
 
 farToCel(tempInput);
 
+
+
+
+
 //Pt. Two
 var addNumbers = [123, 4, 23, 1, 10];
-var sum;
+
 
 function sumNums(arr) {
+  var sum = 0;
 
-  for (var i = 0; i < arry.length; i++) {
+  for (var i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
 
+  return sum;
 }
 
-sum(addNumbers);
+var total = sumNums(addNumbers);
+console.log("The sum of the number is " + total);
+
+
+
+
 
 //Pt. Three
-var bigNumbers[];
+var numbers = [2, 123, 4, 23, 167, 1, 10];
 
-function sum(arr) {
 
-  for (var i = 0; i < arry.length; i++) {
-    i
+function greatestNumber(arr) {
+  var largest = 0;
+  //if all values are positive
+
+  for (var j in arr) {
+    var currentNum = arr[j];
+    if (currentNum > largest) {
+      largest = currentNum;
+    }
   }
 
+  return largest;
+}
+
+var largestNum = greatestNumber(numbers);
+console.log(largestNum);
+
+
+
+
+
+//Pt. Four
+var myString = "hello, it's me.";
+
+function charFreq(str) {
+  var myChar = {};
+
+  for (var k = 0; k < str.length; k++) {
+    var currentChar = str.charAt(k);
+
+    if (myChar[currentChar] === undefined) {
+      myChar[currentChar] = 0;
+    }
+
+    myChar[currentChar]++;
+  }
+
+  return myChar;
+
+}
+
+var frequency = charFreq(myString);
+
+for (var l in frequency) {
+  console.log(l + ": " + frequency[l]);
 }
